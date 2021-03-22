9889a956d6c83feba8c591ee0adfad4690e73804
Miguel Angel Tomairo <matomairo@gmail.com>
2021-03-22 18:04:33 -0500

    clean files

--- src/app.py
+++ src/app.py
@@ -37,43 +37,6 @@ class TaskSchema(ma.Schema):
 task_schema = TaskSchema()
 tasks_schema = TaskSchema(many=True)
 
-# @app.route('/tasks', methods=['POST'])
-# def create_task():
-#     title = request.json['title']
-#     description = request.json['description']
-#     new_task = Task(title,description)
-#     db.session.add(new_task)
-#     db.session.commit()
-#     return task_schema.jsonify(new_task)
-
-# @app.route('/tasks', methods=['GET'])
-# def get_task():
-#     all_tasks = Task.query.all()
-#     result = tasks_schema.dump(all_tasks)
-#     return jsonify(result)
-
-# @app.route('/tasks/<id>', methods=['GET'])
-# def get_task_byId(id):
-#     task = Task.query.get(id)
-#     return task_schema.jsonify(task)
-
-# @app.route('/tasks/<id>', methods=['PUT'])
-# def update_task(id):
-#     task = Task.query.get(id)
-#     title = request.json['title']
-#     description = request.json['description']    
-#     task.title = title
-#     task.description = description
-#     db.session.commit()
-#     return task_schema.jsonify(task)
-
-# @app.route('/tasks/<id>', methods=['DELETE'])
-# def delete_task(id):
-#     task = Task.query.get(id)
-#     db.session.delete(task)
-#     db.session.commit()
-#     return task_schema.jsonify(task)
-
 @app.route('/', methods=['GET'])
 def index():
     return jsonify({"message":"Bienvenidos al API"})
